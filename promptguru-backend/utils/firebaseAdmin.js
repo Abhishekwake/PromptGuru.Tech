534025ff4f4692573792f03793f0e0a65d7306db
Abhishek Wake <abhishekwake111@gmail.com>
2025-07-29 15:49:45 +0530

    solve firebaseAdmin local issue

--- promptguru-backend/utils/firebaseAdmin.js
+++ promptguru-backend/utils/firebaseAdmin.js
@@ -1,9 +1,6 @@
 import admin from 'firebase-admin';
-import path from 'path';
-import { readFileSync } from 'fs';
 
-const serviceAccountPath = path.resolve('F:/PromptGuru/prompguru-firebase-adminsdk-fbsvc-749f5b5eb0.json');
-const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf8'));
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_JSON);
 
 if (!admin.apps.length) {
   admin.initializeApp({
@@ -12,3 +9,20 @@ if (!admin.apps.length) {
 }
 
 export default admin;
+
+
+//before local wlaa
+// import admin from 'firebase-admin';
+// import path from 'path';
+// import { readFileSync } from 'fs';
+
+// const serviceAccountPath = path.resolve('F:/PromptGuru/prompguru-firebase-adminsdk-fbsvc-749f5b5eb0.json');
+// const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf8'));
+
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// }
+
+// export default admin;
